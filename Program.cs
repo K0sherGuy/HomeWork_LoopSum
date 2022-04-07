5a6a5ddfdf6b6a0d506d0149d1563a2d4c6fc031
Daniil <daniil.goihinberg@gmail.com>
2022-04-07 22:13:16 +0300

    my homework

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LoopSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int random = 0;
+            int sum = 0;
+         
+            
+            for (int i = 0; i < 10; i++)
+            {
+                random = rnd.Next(1, 11);
+                sum = random + sum;
+                Console.WriteLine($"Random number {i+1} is {random}");
+            }
+
+            Console.WriteLine($"Total is {sum}");
+
+
+        }
+    }
+}
